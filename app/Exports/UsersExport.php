22272fc883e77ca9d4d2bd030b2075f567ec8554
mahfuz380 <almahfuz380@gmail.com>
2021-08-08 12:25:57 +0600

    form view done

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -5,28 +5,35 @@ namespace App\Exports;
 use App\User;
 use Illuminate\Contracts\Support\Responsable;
 use Illuminate\Support\Collection;
+use Illuminate\View\View;
 use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\FromArray;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\FromView;
 
 //class UsersExport implements FromCollection
-class UsersExport implements FromArray
+//class UsersExport implements FromArray
+class UsersExport implements FromView
 {
 //    use Exportable;
     //private $fileName = 'Users.xlsx';
     /**
     * @return \Illuminate\Support\Collection
     */
-    public function array() :array
+    public function view() :View
     {
 //        return User::all();
-        return
-            [
-                ['Harish','Harish@Example.com'],
-                ['Mah','mah@Example.com'],
-                ['Khan','Khan@Example.com']
+//        return
+//            [
+//                ['Harish','Harish@Example.com'],
+//                ['Mah','mah@Example.com'],
+//                ['Khan','Khan@Example.com']
+//
+//            ];
 
-            ];
+//        return view('exports.users',[
+//            'users'=>User::all()
+//        ] );
 
     }
 }
