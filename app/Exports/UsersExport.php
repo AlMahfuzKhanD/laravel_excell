baba14b1b57411e44d1d3a6a9f70cd5a8ca61012
mahfuz380 <almahfuz380@gmail.com>
2021-08-08 11:11:36 +0600

    excell download done

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -3,10 +3,14 @@
 namespace App\Exports;
 
 use App\User;
+use Illuminate\Contracts\Support\Responsable;
+use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\FromCollection;
 
-class UsersExport implements FromCollection
+class UsersExport implements FromCollection , Responsable
 {
+    use Exportable;
+    //private $fileName = 'Users.xlsx';
     /**
     * @return \Illuminate\Support\Collection
     */
--- app/Http/Controllers/UsersExportController.php
+++ app/Http/Controllers/UsersExportController.php
@@ -4,12 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Exports\UsersExport;
 use Illuminate\Http\Request;
-use Maatwebsite\Excel\Facades\Excel;
+use Maatwebsite\Excel\Excel;
 
 class UsersExportController extends Controller
 {
+    private $excel;
+    public function __construct(Excel $excel){
+        $this->excel = $excel;
+    }
     public function export()
     {
-        return Excel::download(new UsersExport,'users.xlsx');
+        // return Excel::download(new UsersExport,'users.xlsx');
+        // Exportable
+        // return (new UsersExport)->download('users.xlsx');
+        // Responsable
+        // return new UsersExport;
+        //Excel dependency injection
+
+        //return $excel->download(new UsersExport,'Users.xlsx');
+        //using constructor
+        return $this->excel->download(new UsersExport,'Users.xlsx');
     }
 }
